ce6b42a3b4362b2e6bc9dfec0645f36983789a96
punkushal <spycymagar@gmail.com>
2026-09-12 09:43:42 +0545

    feat(utility): add duration formatter

---
+++ lib/core/utils/formatters.dart
@@ -0,0 +1,5 @@
+String formatDuration(Duration d) {
+  final minutes = d.inMinutes.remainder(60).toString().padLeft(2, '0');
+  final seconds = d.inSeconds.remainder(60).toString().padLeft(2, '0');
+  return '$minutes:$seconds';
+}
